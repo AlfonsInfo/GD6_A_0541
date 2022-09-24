970a5c89b767df96c9857a0041b09b865584107a
Alfonsinfo <itsmealfons@gmail.com>
2022-09-24 12:23:37 +0700

    tambah !! di NotificatioReceiver.kt

--- app/src/main/java/com/alfonsus/gd6_a_0541/NotificationReceiver.kt
+++ app/src/main/java/com/alfonsus/gd6_a_0541/NotificationReceiver.kt
@@ -7,7 +7,7 @@ import android.widget.Toast
 
 class NotificationReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        val message = intent.getStringExtra("toastMessage")
+        val message = intent!!.getStringExtra("toastMessage")
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
 }
